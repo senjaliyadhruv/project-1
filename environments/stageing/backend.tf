9cb8dc6f0bfea55557a0fa23f0fa524602fe9bfb
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-22 13:09:27 +0530

    multi-environments

---
+++ environments/stageing/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "dhruv-project-1204"
+    key            = "staging/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-locks"
+  }
+}
