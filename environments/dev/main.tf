fc896b611061357b511341f0fa3c1f3b2cc4bf95
senjaliyadhruv <senjaliyadhruvr@gmail.com>
2025-07-22 12:16:28 +0530

    dev/prod/staging

---
+++ environments/dev/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source      = "../../modules/vpc"
+  vpc_cidr    = var.vpc_cidr
+  subnet_cidr = var.subnet_cidr
+  az          = var.az
+  name        = "dev-vpc"
+}
+
+module "ec2" {
+  source        = "../../modules/ec2"
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = module.vpc.public.id
+  name          = "dev-ec2"
+}
---
+++ environments/dev/variable.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "az" {
+  default = "us-east-1a"
+}
+
+variable "ami" {
+  default = "ami-053b0d53c279acc90"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
